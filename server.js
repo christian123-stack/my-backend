e0fe5b2a90c5ce5c6dc8066ab45c44297801ca17
christian123-stack <christiannicholassinaga@gmail.com>
2025-12-01 17:51:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ import commentRoute from "./routes/commentRoute.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const JWT_SECRET = process.env.JWT_SECRET;
 const MONGO_URI = process.env.MONGO_URI;
 
